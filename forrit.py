3df2b8dfd5d59b60d004601b0dd5aed53d991d13
eytorinn <eytoralex@gmail.com>
2018-09-04 15:01:21 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -1,1 +1,15 @@
+from bottle import route, run
+
+@route('/')
+def hello():
+    return "Hello World!"
+
+
+@route('/about')
+def about():
+    return
+
+
+
+run(host='https://vefforritun123.herokuapp.com/', debug=True)
 
